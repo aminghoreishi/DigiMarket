93d309bc2773c973cc7d96944804362c96ee57c8
amin <amin.ghoreishi86@gmail.com>
2025-10-24 16:13:54 +0330

    feat: MenuNavbar

--- src/components/module/Header/Buttons.tsx
+++ src/components/module/Header/Buttons.tsx
@@ -4,7 +4,7 @@ function Buttons() {
   return (
     <div className="flex items-center font-danaMed gap-3">
       <div className="flex items-center gap-x-2 border-2 p-2 rounded-xl border-gray-300 cursor-pointer">
-        <p className="text-sm">حساب کاربری</p>
+        <p className="text-xs">حساب کاربری</p>
         <FaRegUser />
       </div>
       <div className="flex items-center border-2 p-2 rounded-xl border-gray-300 gap-x-2 cursor-pointer">
--- src/components/module/HeaderMenu.tsx
+++ src/components/module/HeaderMenu.tsx
@@ -3,7 +3,7 @@ import linkModel from "@/models/link";
 import { LinkType } from "@/types/link.type";
 import Link from "next/link";
 import React from "react";
-
+import { MdKeyboardArrowDown } from "react-icons/md";
 async function HeaderMenu() {
   await db();
 
@@ -16,11 +16,19 @@ async function HeaderMenu() {
   console.log(links);
 
   return (
-    <div className="container mx-auto bg-gray-100 py-3">
-      <ul className=" text-sm">
+    <div className="container mx-auto bg-gray-50 py-5">
+      <ul className="text-xs flex items-center gap-x-3">
         {links.map((link) => (
-          <li className="font-danaMed cursor-pointer" key={link._id}>
-            <Link href={link.href}>{link.title}</Link>
+          <li
+            className="font-danaMed group relative cursor-pointer flex items-center gap-x-2"
+            key={link._id}
+          >
+            <span>
+              <Link href={link.href}>{link.title}</Link>
+            </span>
+            {link.subLink.length !== 0 && <MdKeyboardArrowDown className=" transition-all group-hover:rotate-180" />}
+
+            {link.subLink.length !== 0 && <div className="absolute -bottom-20 invisible opacity-0 group-hover:visible group-hover:opacity-100 bg-white w-[200px] p-3 ">dd</div>}
           </li>
         ))}
       </ul>
