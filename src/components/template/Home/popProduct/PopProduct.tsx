ef5a45203f2956b64e12276d4854fecc8d848902
amin <amin.ghoreishi86@gmail.com>
2025-10-29 16:16:13 +0330

    feat: swiper product

--- src/components/template/Home/popProduct/PopProduct.tsx
+++ src/components/template/Home/popProduct/PopProduct.tsx
@@ -1,10 +1,12 @@
 import TopSubject from '@/components/module/TopSubject/TopSubject'
 import React from 'react'
+import SwiperPop from './SwiperPop'
 
 function PopProduct() {
   return (
     <div>
         <TopSubject title='محصولات پرفروش'/>
+        <SwiperPop/>
     </div>
   )
 }
