4c92c52d83713c37b6dd7b6d82cd82715b6148a4
aminghoreishi <162731664+aminghoreishi@users.noreply.github.com>
2025-10-31 18:58:07 +0330

    fix: adjust MenuMobile component width and transition for improved usability

--- src/components/module/Header/MenuMobile.tsx
+++ src/components/module/Header/MenuMobile.tsx
@@ -26,8 +26,8 @@ function MenuMobile() {
 
       {/* منوی کشویی */}
       <div
-        className={`fixed top-0 bottom-0 right-0 w-64 bg-white shadow-2xl z-50 transition-transform duration-300 ease-in-out ${
-          isOpen ? "translate-x-0" : "translate-x-full"
+        className={`fixed top-0 bottom-0 right-0 w-60 bg-white shadow-2xl z-50 transition-transform duration-300 ease-in-out ${
+          isOpen ? "translate-x-5" : "translate-x-[120%]"
         }`}
       >
         <div className="p-6">
