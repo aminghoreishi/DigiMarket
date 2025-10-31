a992c05510cf5cc4a21e7518e9ad99d01e63a556
aminghoreishi <162731664+aminghoreishi@users.noreply.github.com>
2025-10-31 19:20:13 +0330

    fix: improve layout structure and styling in single product page for better responsiveness

--- src/app/singleProduct/page.tsx
+++ src/app/singleProduct/page.tsx
@@ -5,23 +5,23 @@ import SwiperImage from "@/components/template/SingleProduct/SwiperImage/SwiperI
 
 function page() {
   return (
-    <div className="">
+    <div className="container mx-auto">
       <div className="grid xl:grid-cols-12 gap-5 mt-5">
-        <div className="col-span-9">
+        <div className="xl:col-span-9">
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-5">
             <div>
               <div>
-                <SwiperImage/>
+                <SwiperImage />
               </div>
             </div>
-            <div className="container mx-auto">
+            <div className="">
               <BredCrumbs />
-              <Info/>
+              <Info />
             </div>
           </div>
         </div>
-        <div className="col-span-3">
-          <Cart/>
+        <div className="  xl:col-span-3">
+          <Cart />
         </div>
       </div>
     </div>
