f78e1f8482065e6c9007e905b180b991d8c6b0e2
aminghoreishi <162731664+aminghoreishi@users.noreply.github.com>
2025-11-02 12:20:02 +0330

    fix: enhance responsiveness of Description and Specifications components with max-sm classes

--- src/components/template/SingleProduct/MainContainer/Specifications.tsx
+++ src/components/template/SingleProduct/MainContainer/Specifications.tsx
@@ -3,26 +3,26 @@ import React from "react";
 function Specifications() {
   return (
     <div className="font-danaMed border-t-2 border-t-zinc-200 pt-5">
-      <h2>مشخصات محصول</h2>
+      <h2 className="max-sm:text-sm">مشخصات محصول</h2>
 
       <div className="mt-5 flex flex-col divide-y-2 divide-zinc-200 space-y-5 ">
         <div className="flex items-center gap-10 pb-5 ">
-          <p className="text-zinc-400 text-sm">پردازنده:</p>
-          <p>
+          <p className="text-zinc-400 max-sm:text-xs text-sm">پردازنده:</p>
+          <p className="max-sm:text-sm">
             AM32x new product AM32x new product AM32x new product AM32x new
             product
           </p>
         </div>
         <div className="flex items-center gap-10 pb-5 ">
-          <p className="text-zinc-400 text-sm">پردازنده:</p>
-          <p>
+          <p className="text-zinc-400 max-sm:text-xs text-sm">پردازنده:</p>
+          <p className="max-sm:text-sm">
             AM32x new product AM32x new product AM32x new product AM32x new
             product
           </p>
         </div>
         <div className="flex items-center gap-10 pb-5">
-          <p className="text-zinc-400 text-sm">پردازنده:</p>
-          <p>
+          <p className="text-zinc-400 max-sm:text-xs text-sm">پردازنده:</p>
+          <p className="max-sm:text-sm">
             AM32x new product AM32x new product AM32x new product AM32x new
             product
           </p>
