7bac470a902e69aeb7192ae032b9b05f2cea81aa
aminghoreishi <162731664+aminghoreishi@users.noreply.github.com>
2025-11-02 18:33:21 +0330

    fix: improve layout and styling of Comment and Comments components for better user experience

--- src/components/template/SingleProduct/MainContainer/Comment/Comments.tsx
+++ src/components/template/SingleProduct/MainContainer/Comment/Comments.tsx
@@ -3,7 +3,9 @@ import Comment from "./Comment";
 
 function Comments() {
   return (
-    <div>
+    <div className="flex flex-col divide-y-2 divide-zinc-400 space-y-5">
+      <Comment />
+      <Comment />
       <Comment />
     </div>
   );
