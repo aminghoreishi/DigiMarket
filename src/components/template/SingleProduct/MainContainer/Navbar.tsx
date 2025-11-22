93fd6aae8662a84351bc192d23adb74c82c58a41
aminghoreishi <amin.ghoreishi86@gmail.com>
2025-11-22 22:11:15 +0000

    feat: implement smooth scrolling to comments in Navbar component

--- src/components/template/SingleProduct/MainContainer/Navbar.tsx
+++ src/components/template/SingleProduct/MainContainer/Navbar.tsx
@@ -1,9 +1,23 @@
-function Navbar() {
+"use client";
+
+
+  function Navbar() {
+
+    const scrollToComment = () => {
+      window.scrollTo({
+        top: 1200,
+        behavior: "smooth",
+      });
+    }
+
+
+
+
   return (
     <ul className="flex max-sm:gap-3 max-sm:text-xs gap-8 border-b-2 border-b-zinc-200 pb-4 mt-8 text-gray-600 font-danaMed cursor-pointer">
       <li>توضیحات</li>
       <li>مشخصات</li>
-      <li>دیدگاه</li>
+      <li onClick={scrollToComment}>دیدگاه</li>
       <li>پرسش ها</li>
     </ul>
   );
